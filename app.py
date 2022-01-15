33a6618ffbdb37d608b1fa9c3891187064131ffc
kentilove095 <6210110616@psu.ac.th>
2022-01-16 02:15:07 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from ast import Return
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello My First Application"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='5000',debug=True)

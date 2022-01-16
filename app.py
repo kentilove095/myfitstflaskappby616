8b4ae14852854abe95363578c301b3a610354804
kentilove095 <6210110616@psu.ac.th>
2022-01-16 23:13:55 +0700

    prefinal_commit

--- app.py
+++ app.py
@@ -7,5 +7,22 @@ app = Flask(__name__)
 def index():
    return render_template('index.html') 
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   error = None
+   
+   if request.method == 'POST':
+      if request.form['username'] != 'admin' or \
+         request.form['password'] != 'admin':
+         error = 'Invalid username or password. Please try again!'
+      else:
+         flash('You were successfully logged in')
+         return redirect(url_for('success'))
+   return render_template('login.html', error = error)
+
+@app.route('/success')
+def success():
+    return render_template("success.html")
+
 if __name__ == '__main__':
    app.run(debug = True)

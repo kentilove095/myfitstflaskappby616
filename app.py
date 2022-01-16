9c87b1a5c4937dc63fcf2bf1e36b147f536f3e4f
kentilove095 <6210110616@psu.ac.th>
2022-01-16 22:41:27 +0700

    6_commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template(url_for('login.html')) 
+    return render_template('login.html') 
   
 @app.route('/success/<name>')
 def success(name):

9a27ea2aed6ccd3f2684f714d7b51c333ecf57d7
kentilove095 <6210110616@psu.ac.th>
2022-01-16 20:35:35 +0700

    1_commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello My First Application"
+def hello_world():
+       return 'hello world'
+app.add_url_rule('/', 'hello', hello_world)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000',debug=True)

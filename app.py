b06ff1a370058fa7e232e59b4eb3c711603b8c77
kentilove095 <6210110616@psu.ac.th>
2022-01-16 23:00:07 +0700

    11_commit

--- app.py
+++ app.py
@@ -6,19 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html') 
-  
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)

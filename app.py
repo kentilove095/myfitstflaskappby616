c26836a5f73fb3ea87c94988c8cdbc6c22d0effa
kentilove095 <6210110616@psu.ac.th>
2022-01-16 23:48:33 +0700

    LLLLL_commit

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def login():
 
 @app.route('/success')
 def success():
-    return render_template("success.html")
+    return render_template('success.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
